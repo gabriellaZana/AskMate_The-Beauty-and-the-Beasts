97e5efffabdfbb91226a40c1c1e8f2977eb61dcc
Akos Putnoki <putnokiakos@gmail.com>
2017-08-15 13:33:31 +0200

    list added

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, redirect, request, session, url_for
+import common
 
 app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/list")
+def index():
+    database = import_data("database.csv")  # common import
+    return render_template("list.html", database=database)
+
 
 if __name__=="__main__":
     app.secret_key = "whoeventriestoguessthis"
