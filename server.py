d963ee7268b6025a40748ca70eefeb6d258ee6e2
Bence Buczkó <surfinbird245@gmail.com>
2017-08-15 13:38:19 +0200

    import/export done

--- server.py
+++ server.py
@@ -2,7 +2,18 @@ from flask import Flask, render_template, redirect, request, session, url_for
 
 app = Flask(__name__)
 
-if __name__=="__main__":
+
+@app.route('/save-answer', methods=['POST'])
+def route_save_answer():
+    pass
+
+
+@app.route('/question/<question-id>/new-answer')
+def new_answer():
+    pass
+
+
+if __name__ == "__main__":
     app.secret_key = "whoeventriestoguessthis"
     app.run(
         debug=True,
