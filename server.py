5200ed6b7802508e5dd06b58a4d237c84620c2c0
Akos Putnoki <putnokiakos@gmail.com>
2017-08-16 17:29:30 +0200

    test

--- server.py
+++ server.py
@@ -48,9 +48,6 @@ def route_question_page(questionid=None):
     id_pos = questionid
     q_list = common.import_story("data/question.csv")
     a_list = common.import_story("data/answer.csv")
-    # viewcount(questionid, "data/question.csv")
-    print(id_pos)
-    print(q_list)
     return render_template('question.html', q_list=q_list, a_list=a_list, id_pos=id_pos)
 
 
@@ -95,15 +92,6 @@ def route_delete_question(questionid=None):
 @app.route('/question/<questionid>/new-answer')
 def new_answer(questionid):
     return render_template('form.html', form="Answer", data=[questionid,"","","","",""])
-
-
-def viewcount(questionid, filename):
-    table = common.import_story(filename)
-    table[int(questionid)-1][2] = int(table[int(questionid)-1][2])
-    table[int(questionid)-1][2] += 1
-    common.export_story(filename, table)
-    return
-
 
 
 if __name__ == "__main__":
