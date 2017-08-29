27c8b16e2035d4749bfa7eecd7abe529928fa618
Akos Putnoki <putnokiakos@gmail.com>
2017-08-29 13:39:39 +0200

    connection handler added

--- common.py
+++ common.py
@@ -1,6 +1,9 @@
 import base64
 import csv
 import time
+from config import Config
+import psycopg2
+import psycopg2.extras
 from datetime import datetime
 
 
@@ -93,3 +96,25 @@ def sortbynumber(index=0):
             line[index] = int(line[index])
         table.sort(key=lambda x: x[index], reverse=True)
     export_story("data/question.csv", table)
+
+
+def open_database():
+    try:
+        connection_string = Config.DB_CONNECTION_STR
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
+    except psycopg2.DatabaseError as exception:
+        print(exception)
+    return connection
+
+
+def connection_handler(function):
+    def wrapper(*args, **kwargs):
+        connection = open_database()
+        # we set the cursor_factory parameter to return with a dict cursor (cursor which provide dictionaries)
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
+        connection.close()
+        return ret_value
+    return wrapper
