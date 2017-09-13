3b5c9702c953ec07928fa98e2aa97818bd9ca4d1
Pataki Zoltan <zolee7777@gmail.com>
2017-09-13 12:11:56 +0200

    question link added to answers

--- server.py
+++ server.py
@@ -409,8 +409,10 @@ def user_activity(user_id):
                                     WHERE id=%s""", (user_id,))
     questions = common.query_handler("""SELECT * FROM question
                                         WHERE users_id=%s""", (user_id,))
-    answers = common.query_handler("""SELECT * FROM answer
-                                      WHERE users_id=%s""", (user_id,))
+    answers = common.query_handler("""SELECT answer.submission_time, answer.vote_number,answer.question_id, answer.message, answer.image, question.title AS quest
+                                      FROM answer
+                                      JOIN question ON question_id=question.id
+                                      WHERE answer.users_id=%s""", (user_id,))
     comments = common.query_handler("""SELECT * FROM comment
                                        WHERE users_id=%s""", (user_id,))
     return render_template("form_user.html", questions=questions, answers=answers, comments=comments, users=users)
