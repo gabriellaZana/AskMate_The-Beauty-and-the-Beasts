df9ff20b6b6b1c47bb6994dc817a2bee95536b44
Bence Buczkó <surfinbird245@gmail.com>
2017-09-13 09:22:45 +0200

    minor fix on design

--- server.py
+++ server.py
@@ -305,10 +305,14 @@ def new_tag(questionid):
 @app.route('/save-tag', methods=['POST'])
 def route_save_tag():
     if request.form["tags"] == "None":
-        common.query_handler("""INSERT INTO tag (name) VALUES(%s)""", (request.form['Tag'],))
         tagid = common.query_handler('SELECT * FROM tag WHERE name=%s', (request.form['Tag'],))
-        common.query_handler("""INSERT INTO question_tag (question_id, tag_id) VALUES(%s, %s)""",
-                             (request.form['question_id'], tagid[0]['id']))
+        if tagid[0]['name'] != request.form['Tag']:
+            common.query_handler("""INSERT INTO tag (name) VALUES(%s)""", (request.form['Tag'],))
+        try:
+            common.query_handler("""INSERT INTO question_tag (question_id, tag_id) VALUES(%s, %s)""",
+                                 (request.form['question_id'], tagid[0]['id']))
+        except:
+            pass
     else:
         tagid = common.query_handler('SELECT * FROM tag WHERE name=%s', (request.form['tags'],))
         try:
@@ -348,68 +352,6 @@ def sort_questions(condition, direction):
             sort = False
             database = common.query_handler("SELECT * FROM question ORDER BY vote_number DESC")
     return render_template("list.html", database=database, search=search, sort=sort)
-
-
-@app.route("/form_user/<user_id>/sort/<condition>/<direction>")
-def sort_user_questions(condition, direction, user_id):
-    sort = True
-    search = False
-    if condition == "time":
-        if direction == "ASC":
-            database = common.query_handler("""SELECT * FROM question
-                                               WHERE users_id=%s
-                                               ORDER BY submission_time
-                                               ASC""", (user_id,))
-        elif direction == "DESC":
-            sort = False
-            database = common.query_handler("""SELECT * FROM question
-                                               WHERE users_id=%s
-                                               ORDER BY submission_time
-                                               DESC""", (user_id,))
-    elif condition == "view":
-        if direction == "ASC":
-            database = common.query_handler("""SELECT * FROM question
-                                               WHERE users_id=%s
-                                               ORDER BY view_number
-                                               ASC""", (user_id,))
-        elif direction == "DESC":
-            sort = False
-            database = common.query_handler("""SELECT * FROM question
-                                               WHERE users_id=%s
-                                               ORDER BY view_number
-                                               DESC""", (user_id,))
-    elif condition == "vote":
-        if direction == "ASC":
-            database = common.query_handler("""SELECT * FROM question
-                                               WHERE users_id=%s
-                                               ORDER BY vote_number
-                                               ASC""", (user_id,))
-        elif direction == "DESC":
-            sort = False
-            database = common.query_handler("""SELECT * FROM question
-                                               WHERE users_id=%s
-                                               ORDER BY vote_number
-                                               DESC""", (user_id,))
-    answers = common.query_handler("""SELECT * FROM answer
-                                      WHERE users_id=%s""", (user_id,))
-    comments = common.query_handler("""SELECT * FROM comment
-                                       WHERE users_id=%s""", (user_id,))
-    users = common.query_handler("""SELECT * FROM users
-                                    WHERE id=%s""", (user_id,))
-    return render_template("form_user.html", questions=database, sort=sort, anwers=answers, comments=comments, users=users)
-
-
-@app.route("/user/<user_id>")
-def user_activity(user_id):
-    users = common.query_handler("""SELECT * FROM users
-                                    WHERE id=%s""", (user_id,))
-    questions = common.query_handler("""SELECT * FROM question
-                                        WHERE users_id=%s""", (user_id,))
-    answers = common.query_handler("""SELECT * FROM answer
-                                      WHERE users_id=%s""", (user_id,))
-    comments = common.query_handler("""SELECT * FROM comment
-                                       WHERE users_id=%s""", (user_id,))
-    return render_template("form_user.html", questions=questions, answers=answers, comments=comments, users=users)
 
 
 if __name__ == "__main__":
